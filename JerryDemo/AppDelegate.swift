0a24850c88565ee77523809f7984db01b316bcb0
Jerry <15323862856@163.com>
2020-12-01 10:41:06 +0800

    添加注释

--- JerryDemo/AppDelegate.swift
+++ JerryDemo/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let nav = UINavigationController(rootViewController: DisplayController())
 
         self.window?.rootViewController = nav
-        
+        //修改
         return true
     }
 
